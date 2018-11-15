70919ae8b105a11241e822089f4b3885a1c697b1
lijiansong <lijiansong@ict.ac.cn>
2018-11-15 15:17:14 +0800

    format csc

--- src/csc.cpp
+++ src/csc.cpp
@@ -10,20 +10,17 @@
 void csc(const int &N, double *nz_vals, int *row_index, int *column_start,
          double *x, double *y) {
   int i, j;
-  double tmp;
 #ifdef CSC_OMP
 #pragma omp parallel for default(shared) private(i, j, tmp)
 #endif // CSC_OMP
   // loop over the columns of sparse matrix
   for (i = 0; i < N; ++i) {
-    tmp = 0.0;
 #ifdef CSC_OMP
     // hint compiler to ignore vector dependencies
     #pragma IVDEP
 #endif
     for (j = column_start[i]; j < column_start[i + 1]; ++j) {
-      tmp += nz_vals[j] * x[i];
+      y[row_index[j]] += nz_vals[j] * x[i];
     }
-    y[row_index[j]] += tmp;
   }
 }
